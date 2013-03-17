254802702dd6cbe37caf5f8b74795486bcd61ba3
Alan Gutierrez <alan@prettyrobots.com>
2013-03-17 00:55:21 -0400

    Notion of what a script would look like.

---
+++ t/register/fixtures/hello.t.js
@@ -0,0 +1,14 @@
+respond(function (step, response) {
+  var fs = require('fs');
+
+  step(function () {
+
+    fs.readFile(__filename, step()); 
+
+  }, function (body) {
+
+    response.setHeader("Content-Type", "text/plain");
+    response.end(body);
+
+  });
+});
