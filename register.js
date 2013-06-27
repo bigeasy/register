856246457fd09438ba11bbd03bab1987f616344d
Alan Gutierrez <alan@prettyrobots.com>
2013-06-27 15:53:25 -0400

    Create an argument parser.
    See #25.

--- register.js
+++ register.js
@@ -12,3 +12,19 @@ exports.createServer = function (port, probe, callback) {
 
   server.listen(port, '127.0.0.1');
 }
+
+exports.argParser = function (path, args) {
+  args = args.slice();
+  var url = require('url'), parsed;
+  if (/^\s*\//.test(args[0]) || /^[^=:]+:/.test(args[0])) {
+    parsed = url.parse(args.shift(), true);
+  } else {
+    parsed = url.parse(path, true);
+  }
+  args.forEach(function (pair) {
+    var $ = /^([^=])*(?:=(.*))$/.exec(pair);
+    parsed.query[$[1]] = $[2];
+  });
+  delete parsed.search;
+  return url.parse(url.format(parsed), true);
+}
