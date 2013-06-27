f69ecca5b8aa97e639525ac793703c0678e5f926
Alan Gutierrez <alan@prettyrobots.com>
2013-06-26 20:14:05 -0400

    Upgrade Proof to 0.0.30.
    Closes #21.

--- t/register/service.t.js
+++ t/register/service.t.js
@@ -3,7 +3,6 @@
 require('proof')(1, function (step, equal) {
   var connect = require('connect'), http = require('http'), path = require('path'), app;
   step(function () {
-    var on = step('on');
     app = connect()
       .use(require('../../service').create(path.join(__dirname, 'fixtures')))
       .listen(8082);
@@ -11,13 +10,13 @@ require('proof')(1, function (step, equal) {
   }, function () {
     var on = step('on');*/
     var req = http.get("http://127.0.0.1:8082/hello");
-    on(req, 'response');
-    on(req, 'error');
+    req.on('response', step.event());
+    req.on('error', step.error());
   }, function (message) {
-    var on = step('on');
     message.setEncoding('utf8');
-    on(message, 'data', []);
-    on(message, 'end');
+    message.on('data', step.event([]));
+    message.on('end', step.event());
+    message.on('error', step.error());
   }, function (data) {
     equal(data.join(''), 'Hello, World!\n', 'connect');
     app.close();
