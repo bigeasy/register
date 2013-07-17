a5cb4dbd38f56f219918ec0056efe05ef627a758
Alan Gutierrez <alan@prettyrobots.com>
2013-07-16 22:15:31 -0400

    Fix `mock.t.js`.
    Changed the results of the `request` mock function without updating all
    the tests that depend upon it.
    
    See #9.

--- t/register/mock.t.js
+++ t/register/mock.t.js
@@ -5,13 +5,13 @@ var request = require('../../request')(__dirname)
 require('proof')(4, function (step, deepEqual, say) {
     step(function () {
         request('/fixtures/hello', {}, step())
-    }, function (headers, body) {
+    }, function (statusCode, headers, body) {
         body.setEncoding('utf8')
         deepEqual(headers['content-type'], 'text/plain', 'hello header')
         deepEqual(body.read(), 'Hello, World!\n', 'hello body')
     }, function () {
         request('/fixtures/params', { a: 1 }, step())
-    }, function (headers, body) {
+    }, function (statusCode, headers, body) {
         body.setEncoding('utf8')
         deepEqual(headers['content-type'], 'text/plain', 'param header')
         deepEqual(JSON.parse(body.read()), { a: 1 }, 'param body')
