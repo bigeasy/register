c85338a2b1b5a1c21e3aa5e249d0297c85d939c6
Alan Gutierrez <alan@prettyrobots.com>
2013-09-05 00:52:06 +0000

    Invoke `raise` without headers.
    Created a test to invoke `raise` without specifying headers.
    
    Closes #62.

--- t/register/fixtures/raise.cgi.js
+++ t/register/fixtures/raise.cgi.js
@@ -1,3 +1,10 @@
-on.get(function (raise) {
-    raise(403, { 'X-NastyGram': 'Go away!' })
+on.get(function (request, raise) {
+    if (request.query.header) {
+        var $ = request.query.header.split(/=/)
+        var headers = {}
+        headers[$[0]] = $[1]
+        raise(403, headers)
+    } else {
+        raise(403)
+    }
 })
--- t/register/raise.t.js
+++ t/register/raise.t.js
@@ -1,11 +1,17 @@
-require('proof')(3, function (step, equal, say) {
+require('proof')(5, function (step, equal, say) {
     var request = require('../../request')(__dirname)
     step(function () {
-        request('fixtures//raise', {}, step())
+        request('fixtures//raise', { header: 'X-NastyGram=Go away!' }, step())
     }, function (statusCode, headers, body) {
         equal(statusCode, 403, 'status code')
         equal(headers['x-nastygram'], 'Go away!', 'headers')
         body.setEncoding('utf8')
         equal(body.read(), '403 Forbidden\n', 'body')
+    }, function () {
+        request('fixtures//raise', {}, step())
+    }, function (statusCode, headers, body) {
+        equal(statusCode, 403, 'status code no headers')
+        body.setEncoding('utf8')
+        equal(body.read(), '403 Forbidden\n', 'body no headers')
     })
 })
