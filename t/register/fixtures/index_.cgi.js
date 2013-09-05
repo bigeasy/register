779823da9d3df275dcecf361f93bcdea21dad032
Alan Gutierrez <alan@prettyrobots.com>
2013-09-05 02:58:20 +0000

    Stop when middleware sends headers.
    Stop processing matched paths when Sencha Connect middleware sends
    headers.
    
    Closes #66.

--- t/register/fixtures/index_.cgi.js
+++ t/register/fixtures/index_.cgi.js
@@ -1,4 +1,6 @@
 var connect = require('connect')
 var query = connect.query()
+var path = require('path')
+var files = connect.static(path.join(__dirname, 'public'))
 
-on.get(function (middleware, step) { middleware(query, step()) })
+on.get(function (middleware, step) { middleware(query, files, step()) })
--- t/register/middleware.t.js
+++ t/register/middleware.t.js
@@ -1,8 +1,11 @@
-require('./proof')(1, function (step, once, equal) {
-    var stream = require('stream')
+require('./proof')(3, function (step, once, equal) {
     step(function () {
         once(step, 'fixtures//errorware', [])
     }, function (response) {
-        equal(response.statusCode, 403, 'status code')
+        equal(response.statusCode, 403, 'error status code')
+        once(step, 'fixtures//hello.txt', [])
+    }, function (response) {
+        equal(response.statusCode, 200, 'static status code')
+        equal(response.body, 'Hello, World!\n', 'body')
     })
 })
