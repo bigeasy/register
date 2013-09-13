c84eb600109160a144041b97758e9490eb89f8f0
Alan Gutierrez <alan@prettyrobots.com>
2013-09-13 03:00:17 +0000

    Implement `PATCH`.
    Closes #50.

--- t/register/fixtures/verb.cgi.js
+++ t/register/fixtures/verb.cgi.js
@@ -7,3 +7,8 @@ on.delete(function (request, response) {
     response.setHeader("Content-Type", "text/plain")
     response.end(JSON.stringify({ method: request.method }))
 })
+
+on.patch(function (request, response) {
+    response.setHeader("Content-Type", "text/plain")
+    response.end(JSON.stringify({ method: request.method }))
+})
--- t/register/verbs.t.js
+++ t/register/verbs.t.js
@@ -1,4 +1,4 @@
-require('./proof')(6, function (step, once, deepEqual, ok) {
+require('./proof')(8, function (step, once, deepEqual, ok) {
     var stream = require('stream')
     step(function () {
         var input = new stream.PassThrough()
@@ -18,6 +18,12 @@ require('./proof')(6, function (step, once, deepEqual, ok) {
         once(step, 'fixtures//verb', ['delete'], input)
     }, function (response) {
         deepEqual(response.headers['content-type'], 'text/plain', 'content type')
-        deepEqual(JSON.parse(response.body), { method: "DELETE" }, 'body')
+        deepEqual(JSON.parse(response.body), { method: 'DELETE' }, 'body')
+        var input = new stream.PassThrough()
+        input.end('a=1', 'utf8')
+        once(step, 'fixtures//verb', ['patch'], input)
+    }, function (response) {
+        deepEqual(response.headers['content-type'], 'text/plain', 'content type')
+        deepEqual(JSON.parse(response.body), { method: 'PATCH' }, 'body')
     })
 })
